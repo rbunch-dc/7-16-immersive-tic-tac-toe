a64e11c798f318f102710e185da6ac739fc37ed2
Robert Bunch <rob@digitalcrafts.com>
2016-07-22 10:45:07 -0400

    Build winner array. Still need to make board

--- scripts.js
+++ scripts.js
@@ -1,9 +1,35 @@
 // N Size Version
 var whosTurn = 1; //start off on player 1's turn
 
+var alph = ['a','b','c','d','e','f','g','h','i'];
 var winners = [];
+var gridSize = 5;
+
+// a0,a1,a2,a3,a4,...aN
+// b0,b1,b2,b3,b4,...bN
+
+var diag1 = [];
+var diag2 = [];
 
 // 1. Build a winners array
+for(var i = 0; i<gridSize; i++){
+	diag1.push(alph[i] + (gridSize-i)); //Diag 1
+	diag2.push(alph[i] + i); //Diag 2
+	var winnersInsideH = [];
+	var winnersInsideV = [];
+	for(var j = 0; j<gridSize; j++){
+		//Vertical Winners
+		winnersInsideV.push(alph[j] + i);
+		//Horizontal Winners
+		winnersInsideH.push(alph[i] + j);
+	}
+	winners.push(winnersInsideH);
+	winners.push(winnersInsideV);
+}
+
+winners.push(diag1);
+winners.push(diag2);
+
 // 2. We need to populate the board
 
 var player1 = []; //Array where we will stash the squares player1 has checked
